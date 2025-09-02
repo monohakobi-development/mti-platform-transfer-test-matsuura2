74f2fd372029cfb30f910a3d7952a8c348333c79
Kaichi Matsuura (MTI) <kaichi_matsuura@monohakobi.com>
2025-09-02 11:57:57 +0900

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -7,5 +7,6 @@ class Program
         // Output "Hello, World!" to the console
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World! Branch-2");
+        Console.WriteLine("Hello, World! Branch-5");
     }
 }
